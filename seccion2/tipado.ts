e4b34908f63172f1b2f6fb09ff93c9c07bd4b817
Alan Luna <aklt1994@gmail.com>
2021-04-03 01:13:35 +0200

    tipado

---
+++ seccion2/tipado.ts
@@ -0,0 +1,25 @@
+/* String */
+let nombre:string = 'Alan'
+
+let apellido:string
+apellido = 'Luna'
+
+/* Boleanos */
+let si:boolean = true
+let no:boolean = false
+
+/* Numbre */
+let numero:number = 10
+numero = 26
+
+/*  Array  */
+let numeros: number[] = [1, 2, 3, 4]
+let nombres: string[] = ['Alan', 'Daniel']
+
+let collecionnumero: Array<number> = [1, 2, 3, 4]
+let collecionnombre: Array<string> = ['Alan', 'Daniel']
+let siono: boolean[] = [true, false, true]
+
+/*  Tupla  */
+let tupla: [number, string, boolean]
+tupla = [26, 'Alan', true]
